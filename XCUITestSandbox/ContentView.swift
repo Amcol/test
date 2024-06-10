3d4814e6c9d259f0e964eb102284f28ada662b18
Oleksii Petruk <amcol@macpaw.com>
2024-06-10 14:43:11 +0300

    Initial Commit

---
+++ XCUITestSandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCUITestSandbox
+//
+//  Created by Amcol on 10.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCUITestSandbox/XCUITestSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCUITestSandboxApp.swift
+//  XCUITestSandbox
+//
+//  Created by Amcol on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct XCUITestSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
