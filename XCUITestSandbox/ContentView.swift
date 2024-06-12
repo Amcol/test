4e1c50471647ffa5d8bd75a9eb879b7d3dced07e
Oleksii Petruk <amcol@macpaw.com>
2024-06-12 17:16:45 +0300

    CI init

--- XCUITestSandbox/ContentView.swift
+++ XCUITestSandbox/ContentView.swift
@@ -8,17 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textFieldText: String = ""
+    @State private var sliderValue: Double = 0.5
+    @State private var selectedSegment: Int = 0
+    @State private var showAlert: Bool = false
+    @State private var alertMessage: String = ""
+    @State private var navigationTitle: String = "Home"
+    @State private var toggleState: Bool = false
+    @State private var stepperValue: Int = 0
+    @State private var selectedDate: Date = Date()
+    @State private var showText: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Spacer()
+            // Button to show text
+            Button(action: {
+                showText = true
+            }) {
+                Text("Show Text")
+                    .padding()
+            }
+            .cornerRadius(8)
+            .padding()
+            .accessibilityIdentifier("ShowTextButton")
+
+            if showText {
+                Text("Hello, World!")
+                    .accessibilityIdentifier("HelloWorldText")
+            }
+
+            // Toggle
+            Toggle(isOn: $toggleState) {
+                Text("Toggle State: \(toggleState ? "On" : "Off")")
+            }
+            .padding()
+            .accessibilityIdentifier("Toggle")
+
+            // Date Picker
+            DatePicker("Select a date", selection: $selectedDate, displayedComponents: .date)
+                .padding()
+                .accessibilityIdentifier("DatePicker")
+
+            // Buttons
+            HStack(spacing: 20) {
+                Button(action: {
+                    showAlert = true
+                    alertMessage = "You pressed the Red button"
+                }) {
+                    Text("Red")
+                        .padding()
+                }
+                .background(.red)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .accessibilityIdentifier("RedButton")
+
+                Button(action: {
+                    showAlert = true
+                    alertMessage = "You pressed the Green button"
+                }) {
+                    Text("Green")
+                        .padding()
+                }
+                .background(.green)
+                .cornerRadius(8)
+                .accessibilityIdentifier("GreenButton")
+
+                Button(action: {
+                    showAlert = true
+                    alertMessage = "You pressed the Blue button"
+                }) {
+                    Text("Blue")
+                        .padding()
+                }
+                .background(.blue)
+                .cornerRadius(8)
+                .accessibilityIdentifier("BlueButton")
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Button Pressed"),
+                    message: Text(alertMessage),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
+            .padding()
+
+            Spacer()
         }
-        .padding()
+        .navigationTitle(navigationTitle)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- XCUITestSandboxUITests/XCUITestSandboxUITests.swift
+++ XCUITestSandboxUITests/XCUITestSandboxUITests.swift
@@ -1,41 +1,63 @@
-//
-//  XCUITestSandboxUITests.swift
-//  XCUITestSandboxUITests
-//
-//  Created by Amcol on 10.06.2024.
-//
-
 import XCTest
 
-final class XCUITestSandboxUITests: XCTestCase {
+class UITestProjectUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    override func tearDownWithError() throws {}
+
+
+    func testToggle() throws {
+        let app = XCUIApplication()
+        let toggle = app.checkBoxes["Toggle"]
+        
+        toggle.click()
+        XCTAssertTrue(toggle.label.contains("Toggle State: On"))
+    }
+    
+    func testShowTextButton() throws {
+        let app = XCUIApplication()
+        let showTextButton = app.buttons["ShowTextButton"]
+
+        showTextButton.click()
+
+        let helloWorldText = app.staticTexts["HelloWorldText"]
+        XCTAssertTrue(helloWorldText.exists)
+    }
+
+    func testDatePicker() throws {
+        let app = XCUIApplication()
+        let datePicker = app.datePickers["DatePicker"]
+        datePicker.click()
+        
+        XCTAssertTrue(datePicker.exists)
+    }
+
+    func testButtonsAndSheets() throws {
+        let app = XCUIApplication()
+        
+        let redButton = app.buttons["RedButton"]
+        let greenButton = app.buttons["GreenButton"]
+        let blueButton = app.buttons["BlueButton"]
+        let alert = app.sheets["alert"]
+
+        redButton.tap()
+        XCTAssertTrue(alert.exists)
+        XCTAssertTrue(alert.staticTexts["You pressed the Red button"].exists)
+        alert.buttons["OK"].click()
+        
+        greenButton.tap()
+        XCTAssertTrue(alert.exists)
+        XCTAssertTrue(alert.staticTexts["You pressed the Green button"].exists)
+        alert.buttons["OK"].click()
+        
+        blueButton.tap()
+        XCTAssertTrue(alert.exists)
+        XCTAssertTrue(alert.staticTexts["You pressed the Blue button"].exists)
+        alert.buttons["OK"].click()
     }
 }
